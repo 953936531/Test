7c749592e87ae78871856e6657afebd8969c9506
张江彪 <953936531@qq.com>
2019-06-07 16:07:55 +0800

    屏蔽微信JS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -724,39 +724,39 @@
     <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
     <script src="/js/custom.js"></script>
     <script src="https://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
-    <script>
-      let timestamp = (new Date()).valueOf();
-      wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        appId: 'wx2af68763f70f0a4b', // 必填，公众号的唯一标识
-        timestamp: timestamp, // 必填，生成签名的时间戳
-        nonceStr: '123456', // 必填，生成签名的随机串
-        signature: 'jiangbiao',// 必填，签名
-        jsApiList: ['checkJsApi','updateAppMessageShareData','ready'] // 必填，需要使用的JS接口列表
-      });
-      wx.ready(function () {
-        wx.checkJsApi({
-        jsApiList: ['updateAppMessageShareData'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-        success: function(res) {
-          console.log('resresres',res)
-          // 以键值对的形式返回，可用的api值true，不可用为false
-          // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-        }
-      })
-      });
+{{--    <script>--}}
+{{--      let timestamp = (new Date()).valueOf();--}}
+{{--      wx.config({--}}
+{{--        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。--}}
+{{--        appId: 'wx2af68763f70f0a4b', // 必填，公众号的唯一标识--}}
+{{--        timestamp: timestamp, // 必填，生成签名的时间戳--}}
+{{--        nonceStr: '123456', // 必填，生成签名的随机串--}}
+{{--        signature: 'jiangbiao',// 必填，签名--}}
+{{--        jsApiList: ['checkJsApi','updateAppMessageShareData','ready'] // 必填，需要使用的JS接口列表--}}
+{{--      });--}}
+{{--      wx.ready(function () {--}}
+{{--        wx.checkJsApi({--}}
+{{--        jsApiList: ['updateAppMessageShareData'], // 需要检测的JS接口列表，所有JS接口列表见附录2,--}}
+{{--        success: function(res) {--}}
+{{--          console.log('resresres',res)--}}
+{{--          // 以键值对的形式返回，可用的api值true，不可用为false--}}
+{{--          // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}--}}
+{{--        }--}}
+{{--      })--}}
+{{--      });--}}
 
-      wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-        wx.updateAppMessageShareData({
-          title: '张江彪', // 分享标题
-          desc: '一个在上海工作的开发,每天都在成长,PHP是世界上最好的语言;', // 分享描述
-          link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-          imgUrl: '/images/image_1_long.jpg', // 分享图标
-          success: function () {
-            // 设置成功
-          }
-        })
-      });
-    </script>
+{{--      wx.ready(function () {   //需在用户可能点击分享按钮前就先调用--}}
+{{--        wx.updateAppMessageShareData({--}}
+{{--          title: '张江彪', // 分享标题--}}
+{{--          desc: '一个在上海工作的开发,每天都在成长,PHP是世界上最好的语言;', // 分享描述--}}
+{{--          link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致--}}
+{{--          imgUrl: '/images/image_1_long.jpg', // 分享图标--}}
+{{--          success: function () {--}}
+{{--            // 设置成功--}}
+{{--          }--}}
+{{--        })--}}
+{{--      });--}}
+{{--    </script>--}}
 
   </body>
 </html>
